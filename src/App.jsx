6c21a45e1dc385e882fbe75896023b0109246825
hamza raji <hamza.raj@emsi-edu.com>
2025-12-22 15:55:13 +0100

    TD01

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,51 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import logoUniversite from './assets/logo-universite-cote-azur.svg'
 import './App.css'
+
+function Header(){
+  return (
+    <header>
+      <img src={logoUniversite} className="logo" alt="Logo Formation" />
+      <h1>Introduction à React</h1>
+      <h2>A la découverte des premières notions de React</h2>
+    </header>
+  );
+}
+
+function MainContent(){
+  return (
+    <main>
+      <p> :)</p>
+    </main>
+  );
+}
+
+function Footer(){
+  return (
+    <footer style={{ 
+      position: 'fixed', 
+      bottom: 0, 
+      left: 0, 
+      right: 0, 
+      textAlign: 'center', 
+      padding: '1rem',
+      backgroundColor: '#242424'
+    }}>
+      <p>Tous droits réservés - [Raji] [Hamza]</p>
+    </footer>
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   )
 }
